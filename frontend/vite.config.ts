da1b469710c13f454a805ebee274746723dd5ec2
ronmin11 <ronminin@gmail.com>
2025-07-29 17:41:52 -0500

    troubleshoot

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,8 +17,6 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@/lib/utils": path.resolve(__dirname, "./src/lib/utils.ts"),
     },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   },
 }));
